4b9da12989762b4e97562fa2bec9689e186f9484
Casey Ross <caseyross003@gmail.com>
2017-03-24 21:56:36 -0500

    initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# Portfolio
+
+This project consists of html/css/js code for a portfolio website.
+
+## Prerequisites
+
+You will need the following things properly installed on your computer.
+
+* [Git](http://git-scm.com/)
+
+## Installation
+
+* run `git clone https://github.com/caseyr003/portfolio.git`
+
+## Running
+
+Once you have downloaded the project:
+
+* change into the project directory
+* open `index.html`
+
+This will open the web page in your default browser
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    minifyCSS = require('gulp-clean-css'),
+    imageopt = require('gulp-image-optimization');
+
+// Minifies and combines all js files into js/min/app.min.js
+gulp.task('scripts', function(){
+   return gulp.src('js/*.js')
+       .pipe(uglify())
+       .pipe(concat('app.min.js'))
+       .pipe(gulp.dest('js/min/'));
+});
+
+// Minifies and combines all css files into css/min/app.min.css
+gulp.task('styles', function(){
+   return gulp.src('css/*.css')
+       .pipe(minifyCSS())
+       .pipe(concat('app.min.css'))
+       .pipe(gulp.dest('css/min/'));
+});
+
+// Optimizes images into images/opt
+gulp.task('images', function() {
+   return gulp.src('images/*.png')
+              .pipe(imageopt({
+                 optimizationLevel: 5
+              }))
+              .pipe(gulp.dest('images/opt'));
+});
+
+// Start watch for changes to all js and css files
+gulp.task('watch', function(){
+   gulp.watch('js/*.js', ['scripts']);
+   gulp.watch('css/*.css', ['styles']);
+});
+
+// Set default to run all gulp tasks
+gulp.task('default', ['styles', 'scripts', 'images', 'watch']);
